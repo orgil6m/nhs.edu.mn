cdc234738e7701c1080a46d26a54f65cbef5e18b
K1T20 <56916807+bangme4@users.noreply.github.com>
2023-06-08 15:06:53 +0800

    Iredui

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="w-full h-[27rem]">
+        <div className="w-96 h-full bg-white"></div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
